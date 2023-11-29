da0d06d841a4f7e7b4bf4cd005ceabf4ccbca31b
JuanEstebanAparicio <apariciojuanesteban@gmail.com>
2023-11-28 20:19:42 -0500

    primer commit

---
+++ Hashtags/src/main/java/HashPrueba_Map/Hashtags.java
@@ -0,0 +1,19 @@
+
+
+package HashPrueba_Map;
+
+import java.util.HashMap;
+
+
+
+
+public class Hashtags {
+
+    public static void main(String[] args) {
+        
+       HashMap<Integer,String> hash = new HashMap<>();
+       hash.put(1,"rodolfo");
+        
+        System.out.println(hash);
+    }
+}
